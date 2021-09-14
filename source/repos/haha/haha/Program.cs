f50b1cad89e3ea279fb9bea4cc5d18483152f66f
VanBaoDEV69 <vanbaopmdp@gmail.com>
2021-09-14 14:13:25 +0700

    haha

---
+++ source/repos/haha/haha/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // xin chaof cac ban
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+
+        }
+    }
+}
